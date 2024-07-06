5767ed1bea5b043c88ca2f93e8101bead4990a91
Juan Lozada <juanlozadacochaches@gmail.com>
2024-07-05 23:19:45 -0500

    add habitaciones

---
+++ ReservaHotel/src/main/java/Controller/HabitacionController.java
@@ -0,0 +1,15 @@
+ 
+package Controller;
+
+import View.HabitacionesFrame;
+
+
+public class HabitacionController {
+    private HabitacionesFrame frame;
+   /* private Habitacion model;
+    private int usuarioID;
+    
+    public void initController(){
+        frame.getBtnBuscar().addActionListener(e -> buscarHistorial());
+    }*/
+}
