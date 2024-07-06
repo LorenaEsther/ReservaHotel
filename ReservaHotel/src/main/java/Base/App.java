f5397e19574258d7b00410c02e0a9cbe7b5b7995
Lorena Garcia <lorenaamesqueta@gmail.com>
2024-07-05 22:56:09 -0500

    logincontroller

--- ReservaHotel/src/main/java/Base/App.java
+++ ReservaHotel/src/main/java/Base/App.java
@@ -1,11 +1,26 @@
 
 package Base;
 
-public class App {
+public class App implements AppInterface {
     
 
     public static void main(String[] args) {
         
+    }
+
+    @Override
+    public void onLoginSucces(int Usuario, String role) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void onLoginFailed(String username) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void onLogout(int usuarioID) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
