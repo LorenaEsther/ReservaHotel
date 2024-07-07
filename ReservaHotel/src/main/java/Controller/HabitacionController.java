f0f2b95e18582a9eb494cdc30730233ad6ab3873
Juan Lozada <juanlozadacochaches@gmail.com>
2024-07-07 11:37:13 -0500

    create void controller

--- ReservaHotel/src/main/java/Controller/HabitacionController.java
+++ ReservaHotel/src/main/java/Controller/HabitacionController.java
@@ -2,12 +2,14 @@
 package Controller;
 
 import Model.Habitacion;
+import Model.HabitacionCrudModel;
 import View.HabitacionesFrame;
 
 
 public class HabitacionController {
     private HabitacionesFrame frame;
     private Habitacion model; 
+    private HabitacionCrudModel crudModel; 
     
     public void initController(){ 
         frame.getBtnNuevo().addActionListener(e -> agregarHabitacion());
@@ -18,7 +20,14 @@ public class HabitacionController {
     } 
     
     private void cargarHabitaciones(){
+        var data = model.g(usuarioID);
+        HistorialTableModel tableModel = new HistorialTableModel();
         
+        for(String[] row : data){
+            tableModel.addHistorial(row[0], row[1], row[2], row[3]);
+        }
+        
+        frame.getTblHistorial().setModel(tableModel);
     }
     
     private void agregarHabitacion(){
