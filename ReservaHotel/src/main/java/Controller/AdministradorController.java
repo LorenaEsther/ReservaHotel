48048cf62151067712a33f88ed1d88fbd6bab02a
Lorena Garcia <lorenaamesqueta@gmail.com>
2024-07-07 21:56:13 -0500

    admin

---
+++ ReservaHotel/src/main/java/Controller/AdministradorController.java
@@ -0,0 +1,22 @@
+
+package Controller;
+
+//Base 
+import Base.App;
+
+//Model
+import Model.HabitacionCrudModel;
+import Model.LoginModel;
+import Model.ClienteCrudModel;
+
+//View
+import View.AdministradorFrame;
+import View.HabitacionesTableModel;
+import View.LoginFrame;
+import View.HabitacionesFrame;
+import View.ClienteFrame;
+
+
+public class AdministradorController {
+    
+}
